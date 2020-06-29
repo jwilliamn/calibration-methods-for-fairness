7c6116290e9a15a353ffd45275625af4020da3c9
J.Williamn <hwilliamnq@gmail.com>
2020-06-29 03:08:33 +0300

    Metrics for bias estimation

--- utils.py
+++ utils.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 plt.style.use('seaborn-white')
 import seaborn as sns
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+import plotly.figure_factory as ff
+from plotly import tools
+
+from IPython.display import Markdown, display
+
+from aif360.datasets import StandardDataset
+from aif360.metrics import BinaryLabelDatasetMetric, ClassificationMetric
+from aif360.algorithms.preprocessing import LFR, Reweighing
+from aif360.algorithms.inprocessing import AdversarialDebiasing, PrejudiceRemover
+from aif360.algorithms.postprocessing import CalibratedEqOddsPostprocessing, EqOddsPostprocessing, RejectOptionClassification
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, roc_curve, auc
 
 # Get features categorical and numerical
 def get_columns(data, columns):
@@ -61,3 +81,319 @@ def plot_bar(data, col, Y_columns, max_cat=10):
         axs[i].set_title("Number of "+col+" by "+y_col)
 
     plt.show()
+
+def decode_dataset(data, encoders, numerical_features, categorical_features):
+    df = data.copy()
+    for feat in df.columns.values:
+        if feat in numerical_features:
+            df[feat] = encoders[feat].inverse_transform(np.array(df[feat]).reshape(-1, 1))
+    for feat in categorical_features:
+        df[feat] = encoders[feat].inverse_transform(df[feat].astype(int))
+    return df
+
+
+def get_model_performance(X_test, y_true, y_pred, probs):
+    accuracy = accuracy_score(y_true, y_pred)
+    matrix = confusion_matrix(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    preds = probs[:, 1]
+    fpr, tpr, threshold = roc_curve(y_true, preds)
+    roc_auc = auc(fpr, tpr)
+
+    return accuracy, matrix, f1, fpr, tpr, roc_auc
+
+def plot_model_performance(model, X_test, y_true):
+    y_pred = model.predict(X_test)
+    probs = model.predict_proba(X_test)
+    accuracy, matrix, f1, fpr, tpr, roc_auc = get_model_performance(X_test, y_true, y_pred, probs)
+
+    print('Accuracy of the model :', accuracy)
+    print('F1 score of the model :',f1)
+
+    fig = plt.figure(figsize=(15, 6))
+    ax = fig.add_subplot(1, 2, 1)
+    sns.heatmap(matrix, annot=True, cmap='Reds', fmt='g')
+    plt.title('Confusion Matrix')
+
+    ax = fig.add_subplot(1, 2, 2)
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic curve')
+    plt.legend(loc="lower right")
+
+
+def fair_metrics(dataset, pred, pred_is_dataset=False):
+    if pred_is_dataset:
+        dataset_pred = pred
+    else:
+        dataset_pred = dataset.copy()
+        dataset_pred.labels = pred
+    
+    cols = ['statistical_parity_difference', 'equal_opportunity_difference', 'average_abs_odds_difference',  'disparate_impact', 'theil_index']
+    obj_fairness = [[0,0,0,1,0]]
+    
+    fair_metrics = pd.DataFrame(data=obj_fairness, index=['objective'], columns=cols)
+    
+    for attr in dataset_pred.protected_attribute_names:
+        idx = dataset_pred.protected_attribute_names.index(attr)
+        privileged_groups =  [{attr:dataset_pred.privileged_protected_attributes[idx][0]}] 
+        unprivileged_groups = [{attr:dataset_pred.unprivileged_protected_attributes[idx][0]}] 
+        
+        classified_metric = ClassificationMetric(dataset, 
+                                                     dataset_pred,
+                                                     unprivileged_groups=unprivileged_groups,
+                                                     privileged_groups=privileged_groups)
+
+        metric_pred = BinaryLabelDatasetMetric(dataset_pred,
+                                                     unprivileged_groups=unprivileged_groups,
+                                                     privileged_groups=privileged_groups)
+
+        acc = classified_metric.accuracy()
+
+        row = pd.DataFrame([[metric_pred.mean_difference(),
+                                classified_metric.equal_opportunity_difference(),
+                                classified_metric.average_abs_odds_difference(),
+                                metric_pred.disparate_impact(),
+                                classified_metric.theil_index()]],
+                           columns  = cols,
+                           index = [attr]
+                          )
+        fair_metrics = fair_metrics.append(row)    
+    
+    fair_metrics = fair_metrics.replace([-np.inf, np.inf], 2)
+        
+    return fair_metrics
+
+
+def add_to_df_algo_metrics(algo_metrics, model, fair_metrics, preds, probs, name):
+    return algo_metrics.append(pd.DataFrame(data=[[model, fair_metrics, preds, probs]], columns=['model', 'fair_metrics', 'prediction', 'probs'], index=[name]))
+
+
+def plot_fair_metrics(fair_metrics):
+    fig, ax = plt.subplots(figsize=(20,4), ncols=5, nrows=1)
+
+    plt.subplots_adjust(
+        left    =  0.125, 
+        bottom  =  0.1, 
+        right   =  0.9, 
+        top     =  0.9, 
+        wspace  =  .5, 
+        hspace  =  1.1
+    )
+
+    y_title_margin = 1.2
+
+    plt.suptitle("Fairness metrics", y = 1.09, fontsize=20)
+    sns.set(style="dark")
+
+    cols = fair_metrics.columns.values
+    obj = fair_metrics.loc['objective']
+    size_rect = [0.2,0.2,0.2,0.4,0.25]
+    rect = [-0.1,-0.1,-0.1,0.8,0]
+    bottom = [-1,-1,-1,0,0]
+    top = [1,1,1,2,1]
+    bound = [[-0.1,0.1],[-0.1,0.1],[-0.1,0.1],[0.8,1.2],[0,0.25]]
+
+    display(Markdown("### Check bias metrics :"))
+    display(Markdown("A model can be considered bias if just one of these five metrics show that this model is biased."))
+    for attr in fair_metrics.index[1:len(fair_metrics)].values:
+        display(Markdown("#### For the %s attribute :"%attr))
+        check = [bound[i][0] < fair_metrics.loc[attr][i] < bound[i][1] for i in range(0,5)]
+        display(Markdown("With default thresholds, bias against unprivileged group detected in **%d** out of 5 metrics"%(5 - sum(check))))
+
+    for i in range(0,5):
+        plt.subplot(1, 5, i+1)
+        ax = sns.barplot(x=fair_metrics.index[1:len(fair_metrics)], y=fair_metrics.iloc[1:len(fair_metrics)][cols[i]])
+        
+        for j in range(0,len(fair_metrics)-1):
+            a, val = ax.patches[j], fair_metrics.iloc[j+1][cols[i]]
+            marg = -0.2 if val < 0 else 0.1
+            ax.text(a.get_x()+a.get_width()/5, a.get_y()+a.get_height()+marg, round(val, 3), fontsize=15,color='black')
+
+        plt.ylim(bottom[i], top[i])
+        plt.setp(ax.patches, linewidth=0)
+        ax.add_patch(patches.Rectangle((-5,rect[i]), 10, size_rect[i], alpha=0.3, facecolor="green", linewidth=1, linestyle='solid'))
+        plt.axhline(obj[i], color='black', alpha=0.3)
+        plt.title(cols[i])
+        ax.set_ylabel('')    
+        ax.set_xlabel('')
+
+
+
+def get_fair_metrics_and_plot(data, model, plot=True, model_aif=False):
+    pred = model.predict(data).labels if model_aif else model.predict(data.features)
+    # fair_metrics function available in the metrics.py file
+    fair = fair_metrics(data, pred)
+
+    if plot:
+        # plot_fair_metrics function available in the visualisations.py file
+        # The visualisation of this function is inspired by the dashboard on the demo of IBM aif360 
+        plot_fair_metrics(fair)
+        display(fair)
+    
+    return fair
+
+
+def get_attributes(data, selected_attr=None):
+    unprivileged_groups = []
+    privileged_groups = []
+    if selected_attr == None:
+        selected_attr = data.protected_attribute_names
+    
+    for attr in selected_attr:
+            idx = data.protected_attribute_names.index(attr)
+            privileged_groups.append({attr:data.privileged_protected_attributes[idx]}) 
+            unprivileged_groups.append({attr:data.unprivileged_protected_attributes[idx]}) 
+
+    return privileged_groups, unprivileged_groups
+
+
+def plot_fair_metrics_plotly(fair_metrics):
+    bottom = [-1, -1, -1, 0, 0]
+    max_valid = [0.1, 0.1, 0.1, 1.2, 0.25]
+    min_valid = [-0.1, -0.1, -0.1, 0.8, 0]
+    cols = fair_metrics.columns.values
+
+    for i in range(0, 5):
+        col = cols[i]
+
+        x, y = (fair_metrics[col].values, fair_metrics.index)
+        colors = []
+        for v in x:
+            color = '#e74c3c' if v < min_valid[i] or v > max_valid[i] else '#2ecc71'
+            colors.append(color)
+
+        trace = go.Bar(x=x, y=y, marker=dict(color=colors)
+                       , opacity=0.9, orientation='h')
+
+        layout = go.Layout(barmode='group',
+                           title=col,
+                           xaxis=dict(range=[bottom[i], bottom[i] + 2]),
+                           yaxis=go.layout.YAxis(automargin=True),
+                           shapes=[
+                               {
+                                   'type': 'line',
+                                   'x0': min_valid[i],
+                                   'y0': -1,
+                                   'x1': min_valid[i],
+                                   'y1': len(y),
+                                   'line': {
+                                       'color': 'rgb(0, 0, 0)',
+                                       'width': 2,
+                                   },
+                               }, {
+                                   'type': 'line',
+                                   'x0': max_valid[i],
+                                   'y0': -1,
+                                   'x1': max_valid[i],
+                                   'y1': len(y),
+                                   'line': {
+                                       'color': 'rgb(0, 0, 0)',
+                                       'width': 2,
+                                   },
+                               }])
+        fig = go.Figure([trace], layout=layout)
+        py.iplot(fig)
+
+
+def plot_score_fair_metrics(score):
+    display(score.sort_values(['nb_valid', 'score'], ascending=[0, 1]))
+    score.sort_values(['nb_valid', 'score'], ascending=[1, 0], inplace=True)
+
+    gold, silver, bronze, other = ('#FFA400', '#bdc3c7', '#cd7f32', '#3498db')
+    colors = [gold if i == 0 else silver if i == 1 else bronze if i == 2 else other for i in range(0, len(score))]
+    colors = [c for c in reversed(colors)]
+
+    x, y = (score['score'].values, score.index)
+
+    trace = go.Bar(x=x, y=y, marker=dict(color=colors)
+                   , opacity=0.9, orientation='h')
+    layout = go.Layout(barmode='group',
+                       title='Fairest algorithm',
+                       yaxis=go.layout.YAxis(automargin=True))
+    fig = go.Figure([trace], layout=layout)
+    py.iplot(fig)
+    
+
+def score_fair_metrics(fair):
+    objective = [0, 0, 0, 1, 0]
+    max_valid = [0.1, 0.1, 0.1, 1.2, 0.25]
+    min_valid = [-0.1, -0.1, -0.1, 0.8, 0]
+
+    nb_valid = np.sum(((fair.values > min_valid) * (fair.values < max_valid)), axis=1)
+    score = np.sum(np.abs(fair.values - objective), axis=1)
+    score = np.array([score, nb_valid])
+
+    score = pd.DataFrame(data=score.transpose(), columns=['score', 'nb_valid'], index=fair.index)
+    return score
+
+
+def score_all_attr(algo_metrics):
+    attributes = algo_metrics.loc['Origin', 'fair_metrics'].index.values[1:]
+
+    all_scores = np.zeros((len(algo_metrics), 2))
+    for attr in attributes:
+        df_metrics = pd.DataFrame(columns=algo_metrics.loc['Origin', 'fair_metrics'].columns.values)
+        for fair in algo_metrics.loc[:, 'fair_metrics']:
+            df_metrics = df_metrics.append(fair.loc[attr], ignore_index=True)
+        all_scores = all_scores + score_fair_metrics(df_metrics).values
+
+    final = pd.DataFrame(data=all_scores, columns=['score', 'nb_valid'], index=algo_metrics.index)
+    return final
+
+
+def compare_fair_metrics(algo_metrics, attr='Victim Race'):
+    
+    df_metrics = pd.DataFrame(columns=algo_metrics.loc['Origin','fair_metrics'].columns.values)
+    for fair in algo_metrics.loc[:,'fair_metrics']:
+        df_metrics = df_metrics.append(fair.loc[attr], ignore_index=True)
+
+    df_metrics.index = algo_metrics.index.values
+    df_metrics = df_metrics.replace([np.inf, -np.inf], np.NaN)
+    
+    display(df_metrics)
+    plot_fair_metrics_plotly(df_metrics)
+    score = score_fair_metrics(df_metrics)
+    plot_score_fair_metrics(score.dropna())
+
+
+def plot_compare_model_performance(algo_metrics, dataset):
+    X_test = dataset.features
+    y_true = dataset.labels
+    perf_metrics = pd.DataFrame()
+
+    models_name = algo_metrics.index.values
+
+    fig = plt.figure(figsize=(7, 7))
+    plt.title('ROC curve for differents models')
+    lw = 2
+    palette = sns.color_palette("Paired")
+
+    for model_name, i in zip(models_name, range(0, len(models_name))):
+        model = algo_metrics.loc[model_name, 'model']
+
+        if model_name != 'AdvDebiasing':
+            probs = algo_metrics.loc[model_name, 'probs']
+            y_pred = algo_metrics.loc[model_name, 'prediction']
+            accuracy, matrix, f1, fpr, tpr, roc_auc = get_model_performance(X_test, y_true, y_pred, probs)
+
+            perf_metrics = perf_metrics.append(
+                pd.DataFrame([[accuracy, f1]], columns=['Accuracy', 'F1 Score'], index=[model_name]))
+            plt.plot(fpr, tpr, color=palette[i], lw=lw, label=str(model_name) + ' (area = %0.2f)' % roc_auc)
+
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic curve')
+    plt.legend(loc="lower right")
+    display(perf_metrics.sort_values(by=['Accuracy', 'F1 Score'], ascending=[False, False]))
+    plt.show()
+    
+    return perf_metrics
